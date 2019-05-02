2650ea3a3d997d77b7ef6e3db93ba70869226ee6
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-02 22:31:09 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(string = "Squawk!")
-  out = "#{string}"
   puts "#{string}"
-  out
+  out = "#{string}"
 end
